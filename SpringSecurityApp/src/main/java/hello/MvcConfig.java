d8f1a137b91ef73b1e7514b23e5090574647e293
Debadatta Mishra <deba.java@gmail.com>
2018-12-01 14:40:04 +0530

    Xtra

--- SpringSecurityApp/src/main/java/hello/MvcConfig.java
+++ SpringSecurityApp/src/main/java/hello/MvcConfig.java
@@ -16,6 +16,8 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
+        
+        registry.addViewController("/admin").setViewName("admin");
     }
     
     @Bean
--- SpringSecurityApp/src/main/java/hello/WebSecurityConfig.java
+++ SpringSecurityApp/src/main/java/hello/WebSecurityConfig.java
@@ -1,29 +1,63 @@
 package hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.web.FilterInvocation;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 
 @Configuration
 @EnableWebMvcSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	
+	@Bean
+	public RoleHierarchy roleHierarchy(){
+	    RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+	    /* tricks lies here */
+	    roleHierarchy.setHierarchy("ROLE_SUPREME > ROLE_ADMIN ROLE_ADMIN > ROLE_OPERATOR ROLE_OPERATOR > ROLE_GUEST");
+	    return roleHierarchy;
+	}
+	
+	private SecurityExpressionHandler<FilterInvocation>    webExpressionHandler() {
+	    DefaultWebSecurityExpressionHandler defaultWebSecurityExpressionHandler     = new DefaultWebSecurityExpressionHandler();
+	    defaultWebSecurityExpressionHandler.setRoleHierarchy(roleHierarchy());
+	    return defaultWebSecurityExpressionHandler;
+	}
+
+	// https://stackoverflow.com/questions/26661192/spring-boot-spring-security-hierarchical-roles
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated().and().formLogin()
+		
+		
+		http.authorizeRequests().expressionHandler(webExpressionHandler());
+		
+		http.authorizeRequests()
+				.antMatchers("/", "/home").permitAll()
+				.antMatchers("/", "/admin").hasRole("ADMIN")
+				.anyRequest().authenticated().and().formLogin()
 				.loginPage("/login").permitAll().and().logout().permitAll();
 		
-		http.authorizeRequests().antMatchers("/", "/admin").hasAnyRole("ADMIN");
+		//http.authorizeRequests().antMatchers("/", "/admin").hasAnyAuthority("ROLE_ADMIN");
 	}
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
-		auth.inMemoryAuthentication().withUser("deb").password("deb").roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("u").password("u").roles("USER")
+		.and()
+		.withUser("d").password("d").authorities("ROLE_ADMIN");
+		
+//		auth.inMemoryAuthentication().withUser("deb").password("deb").authorities("ROLE_ADMIN");
+//		auth.inMemoryAuthentication().withUser("deb").password("deb").roles("ADMIN");
 	}
 
 	@Override
